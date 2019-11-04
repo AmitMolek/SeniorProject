19e747538cffe3dfe12111f4c32a9071bd2b7c55
Aviv Mahulya <avivmahuliya@gmail.com>
2019-11-04 19:12:31 +0200

    a

--- clientCS/MainWindow.xaml.cs
+++ clientCS/MainWindow.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System.Windows;
 using WebCamService;
-
+using System.IO;
 namespace clientCS
 {
     /// <summary>
@@ -9,7 +9,7 @@ namespace clientCS
     public partial class MainWindow : Window
     {
 
-
+        FtpClient ftp;
         public MainWindow()
         {
             InitializeComponent();
@@ -17,10 +17,37 @@ namespace clientCS
 
         private void btnConnect_Click(object sender, RoutedEventArgs e)
         {
-            FtpClient ftp = new FtpClient();
+           this.ftp = new FtpClient();
             ftp.Login();
-            ftp.Upload(@"C:\לימודים\פרוייקט סוף\image1.jpg");
+            //ftp.Upload(@"C:\לימודים\פרוייקט סוף\image1.jpg");
             //   ftp.Close();
+        }
+        private void btnSendFile_Click(object sender, RoutedEventArgs e)
+        {
+            this.ftp.Upload(@"C:\לימודים\פרוייקט סוף\image1.jpg");
+            //   ftp.Close();
+        }
+
+
+        private void TextBox_DragEnter(object sender, DragEventArgs e)
+        {
+           // e.Effects = DragDropEffects.Copy;
+
+           
+
+            e.Handled = true;
+        }
+
+        private void TextBox_Drop(object sender, DragEventArgs e)
+        {
+            string[] fileloadup = (string[])e.Data.GetData(DataFormats.FileDrop);//Get the filename including path
+            txtPath.Text = File.ReadAllText(fileloadup[0]);//Load data to textBox2
+
+        }
+
+        private void TextBox_Leave(object sender, DragEventArgs e)
+        {
+            e.Handled = true;
         }
     }
 }
