42d8c9cc915b29ed82512aee406f4f6f0339bad7
Aviv Mahulya <avivmahuliya@gmail.com>
2019-11-04 11:59:01 +0200

    c# client added(not working)

---
+++ clientCS/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using WebCamService;
+
+namespace clientCS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            FtpClient ftp = new FtpClient();
+            ftp.Login();
+            ftp.Upload(@"C:\לימודים\פרוייקט סוף\image1.jpg");
+            //   ftp.Close();
+        }
+    }
+}
