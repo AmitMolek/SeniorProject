16c9915d68cebe5b2e795b51e4365fdc36fecb4b
Amit Molek <iamitmolek@gmail.com>
2019-11-12 22:10:49 +0200

    Revert "idk what"
    This reverts commit 51675028bc7fb334a90312a561355a5ff0e9678f.

--- ServerProject/CommunicationHandler.cpp
+++ ServerProject/CommunicationHandler.cpp
@@ -5,8 +5,6 @@
 #include <WinSock2.h>
 #include <vector>
 #include <string>
-#include <boost/algorithm/string/split.hpp>
-#include <boost/algorithm/string/classification.hpp>
 
 constexpr int BUFFER_SIZE = 1024;
 
@@ -31,13 +29,5 @@ int CommunicationHandler::SendBasicMsg(SOCKET& socket, std::string _msg){
 }
 
 void CommunicationHandler::ReceiveFile(SOCKET& socket, std::ofstream& fileStream, FileUploadInfo& fileInfo) {
-	char buffer[BUFFER_SIZE];
-	int receivedBytes = -1;
-
-	while ((receivedBytes = recv(socket, buffer, BUFFER_SIZE, 0)) != -1) {
-		std::string receivedMsg(buffer, buffer + receivedBytes);
-		std::vector<std::string> splitMsg;
-		boost::split(splitMsg, receivedBytes, boost::is_any_of("BOF"));
-
-	}
+	
 }
