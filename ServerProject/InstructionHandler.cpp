a17c6e8981fc385ec2175da47ece0a66a9e3fcb1
Amit Molek <iamitmolek@gmail.com>
2019-11-12 22:10:49 +0200

    Revert "idk what"
    This reverts commit 51675028bc7fb334a90312a561355a5ff0e9678f.

--- ServerProject/InstructionHandler.cpp
+++ ServerProject/InstructionHandler.cpp
@@ -35,8 +35,7 @@ void InstructionHandler::Handle_Pass_data_socket_id(Instruction ins, ConnectionI
 void InstructionHandler::Handle_Pass_file_send(Instruction ins, ConnectionInfo& con) {
 	string fileName = ins.content[0];
 	int fileSize = stoi(ins.content[1]);
-	FileUploadInfo fileUpload(fileName, fileSize);
-
+	
 	ofstream outFile;
-	CommunicationHandler::ReceiveFile(*con.dataSocket, outFile, fileUpload);
+	CommunicationHandler::ReceiveFile(*con.dataSocket, outFile, {fileName, fileSize});
 }
